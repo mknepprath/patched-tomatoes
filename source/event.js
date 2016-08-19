188e4566a61bffb011a2c122f18491ea12e43936
Michael Knepprath <michael@mknepprath.com>
2016-08-19 17:40:20 -0500

    adds gitignore for key and adds updates

--- source/event.js
+++ source/event.js
@@ -1,23 +1,40 @@
-var criticStats = $('#scoreStats').find('div.superPageFontColor').clone()
-var criticScore = criticStats[0].innerText.replace('Average Rating:','').replace('/10','').trim()
-// var criticTitle = $('.scoreTitle')
-// var criticTooltip = $('.scoreTitle > span')
-// console.log(criticTooltip)
-$('span.meter-value > span').html(criticScore*10)
-$('div.progress-bar').css({ width: criticScore*10 + '%' });
+// get critic vars (TODO: check if an average rating is available)
+if ($('#scoreStats').length) {
+  var criticStats = $('#scoreStats').find('div.superPageFontColor').clone()
+  var criticScore = criticStats[0].innerText.replace('Average Rating:','').replace('/10','').trim()
+  var oldCriticScore = $('span.meter-value > span').html()
+  var criticTooltip = $('.tmeterpanel > .scoreTitle > span')
 
-if (criticScore >= 6) {
-  $('span.meter-tomato').removeClass('rotten')
-  $('span.meter-tomato').addClass('fresh')
-  $('div.progress-bar').removeClass('rotten')
-  $('div.progress-bar').addClass('fresh')
-} else {
-  $('span.meter-tomato').removeClass('fresh')
-  $('span.meter-tomato').addClass('rotten')
-  $('div.progress-bar').removeClass('fresh')
-  $('div.progress-bar').addClass('rotten')
+  // update critic section (TODO: add back tooltip with explanation of score)
+  $('.tmeterpanel > .scoreTitle').html('FIXED TOMATOMETER ')
+  // $('.tmeterpanel > .scoreTitle').append(criticTooltip)
+  $('span.meter-value > span').html(criticScore*10)
+  $('div.progress-bar').css({ width: criticScore*10 + '%' })
+  $('#scoreStats').prepend("<div class='superPageFontColor'><span class='subtle superPageFontColor'>Tomatometer: </span>" + oldCriticScore + "%</div>")
+  // hides 'all critics | top critics' options
+  $('.critics-score').parent().hide()
+
+  // update fresh/rotten icon and progress bar color
+  // films that are 'certified fresh' will stay that way regardless
+  if (!$('span.meter-tomato').hasClass('certified_fresh')) {
+    if (criticScore >= 6) {
+      $('span.meter-tomato').removeClass('rotten').addClass('fresh')
+      $('div.progress-bar').removeClass('rotten').addClass('fresh')
+    } else {
+      $('span.meter-tomato').removeClass('fresh').addClass('rotten')
+      $('div.progress-bar').removeClass('fresh').addClass('rotten')
+    }
+  }
 }
 
+// get audience vars
 var audienceStats = $('.audience-info').find('div').clone()
-var audienceScore = audienceStats[0].innerText.replace('Average Rating:','').replace('/5','').trim()
-$('div.meter-value > span').html(audienceScore*10 + '%')
+if (audienceStats[0].innerText.indexOf('Average Rating:') >= 0) {
+  var audienceScore = audienceStats[0].innerText.replace('Average Rating:','').replace('/5','').trim()
+  var oldAudienceScore = $('div.meter-value > span').html()
+
+  // update audience section
+  $('.audiencepanel > .scoreTitle').html('FIXED AUDIENCE SCORE ')
+  $('div.meter-value > span').html(audienceScore*20 + '%')
+  $('.audience-info').prepend("<div class='superPageFontColor'><span class='subtle superPageFontColor'>Audience Score: </span>" + oldAudienceScore + "</div>")
+}
