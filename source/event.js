2b0ba13f34ac93751f1739eb10000128eabb7b5c
m0k013z <mknepprath@gmail.com>
2020-01-17 09:04:47 -0500

    Rewrite to work with redesigned RT

--- source/event.js
+++ source/event.js
@@ -1,40 +1,79 @@
-// get critic vars (TODO: check if an average rating is available)
-if ($('#scoreStats').length) {
-  var criticStats = $('#scoreStats').find('div.superPageFontColor').clone()
-  var criticScore = criticStats[0].innerText.replace('Average Rating:','').replace('/10','').trim()
-  var oldCriticScore = $('span.meter-value > span').html()
-  var criticTooltip = $('.tmeterpanel > .scoreTitle > span')
+// Search script tags for the script that sets global RottenTomatoes variable.
+const scripts = document.querySelectorAll("script");
+const initRTScriptElement = [...scripts].find(script => {
+  return script.innerHTML.includes("/* -- Data -- */");
+});
 
-  // update critic section (TODO: add back tooltip with explanation of score)
-  $('.tmeterpanel > .scoreTitle').html('FIXED TOMATOMETER ')
-  // $('.tmeterpanel > .scoreTitle').append(criticTooltip)
-  $('span.meter-value > span').html(criticScore*10)
-  $('div.progress-bar').css({ width: criticScore*10 + '%' })
-  $('#scoreStats').prepend("<div class='superPageFontColor'><span class='subtle superPageFontColor'>Tomatometer: </span>" + oldCriticScore + "%</div>")
-  // hides 'all critics | top critics' options
-  $('.critics-score').parent().hide()
+if (initRTScriptElement !== null) {
+  // Run the code that adds RottenTomatoes data to `this`.
+  // TODO: I don't know why this isn't already happening, but running it here lets
+  //       me access the data. _shrug_
+  eval(initRTScriptElement.innerHTML);
 
-  // update fresh/rotten icon and progress bar color
-  // films that are 'certified fresh' will stay that way regardless
-  if (!$('span.meter-tomato').hasClass('certified_fresh')) {
-    if (criticScore >= 6) {
-      $('span.meter-tomato').removeClass('rotten').addClass('fresh')
-      $('div.progress-bar').removeClass('rotten').addClass('fresh')
-    } else {
-      $('span.meter-tomato').removeClass('fresh').addClass('rotten')
-      $('div.progress-bar').removeClass('fresh').addClass('rotten')
-    }
+  const scoreInfo = this.RottenTomatoes.context.scoreInfo;
+
+  console.log(scoreInfo);
+
+  // Tomatometer title
+  const tomatometerTitleElement = document.querySelector(
+    ".mop-ratings-wrap__info .mop-ratings-wrap__title"
+  );
+  tomatometerTitleElement.innerHTML = "Critic Average";
+
+  // Tomatometer score
+  const tomatometerScoreElement = document.querySelector(
+    ".mop-ratings-wrap__info .mop-ratings-wrap__percentage"
+  );
+  if (tomatometerScoreElement !== null) {
+    const criticAverageScore = scoreInfo.tomatometerAllCritics.avgScore;
+    tomatometerScoreElement.innerHTML = `${(criticAverageScore * 10)
+      .toFixed(1)
+      .replace(".0", "")}%`;
   }
-}
 
-// get audience vars
-var audienceStats = $('.audience-info').find('div').clone()
-if (audienceStats[0].innerText.indexOf('Average Rating:') >= 0) {
-  var audienceScore = audienceStats[0].innerText.replace('Average Rating:','').replace('/5','').trim()
-  var oldAudienceScore = $('div.meter-value > span').html()
+  // Tomatometer icon
+  const tomatometerIconElement = document.querySelector(
+    ".mop-ratings-wrap__info .mop-ratings-wrap__icon"
+  );
+  if (
+    tomatometerIconElement !== null &&
+    scoreInfo.tomatometerAllCritics.avgScore >= 6
+  ) {
+    tomatometerIconElement.classList.remove("rotten");
+    tomatometerIconElement.classList.add("fresh");
+  } else {
+    tomatometerIconElement.classList.remove("fresh");
+    tomatometerIconElement.classList.add("rotten");
+  }
 
-  // update audience section
-  $('.audiencepanel > .scoreTitle').html('FIXED AUDIENCE SCORE ')
-  $('div.meter-value > span').html(audienceScore*20 + '%')
-  $('.audience-info').prepend("<div class='superPageFontColor'><span class='subtle superPageFontColor'>Audience Score: </span>" + oldAudienceScore + "</div>")
+  // Audience title
+  const audienceTitleElement = document.querySelector(".audience-score__title");
+  audienceTitleElement.innerHTML = "Audience Average";
+
+  // Audience score
+  const audienceScoreElement = document.querySelector(
+    ".audience-score .mop-ratings-wrap__percentage"
+  );
+
+  if (audienceScoreElement !== null) {
+    const audienceAverageScore = scoreInfo.audienceAll.averageRating;
+    audienceScoreElement.innerHTML = `${(audienceAverageScore * 20)
+      .toFixed(1)
+      .replace(".0", "")}%`;
+  }
+
+  // Audience icon
+  const audienceIconElement = document.querySelector(
+    ".audience-score .mop-ratings-wrap__icon"
+  );
+  if (
+    audienceIconElement !== null &&
+    Number(scoreInfo.audienceAll.averageRating) >= 2.5
+  ) {
+    audienceIconElement.classList.remove("spilled");
+    audienceIconElement.classList.add("upright");
+  } else {
+    audienceIconElement.classList.remove("upright");
+    audienceIconElement.classList.add("spilled");
+  }
 }
